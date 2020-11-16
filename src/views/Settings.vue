f4ff251ae1c2b9555493f55f64ff05b72a5c6733
Peggy2344 <peggy102104@gmail.com>
2020-11-16 10:24:25 +0800

    鈴聲設定

---
+++ src/views/Settings.vue
@@ -0,0 +1,55 @@
+<template lang="pug">
+  #settings
+    b-container
+      b-row
+        b-col(cols='12')
+          b-table(:items='items' :fields="fields" @row-clicked="selectAlarm")
+            template(#cell(file)="data")
+              audio(controls :src="'./sounds/'+data.item.file")
+            template(#cell(select)="data")
+</template>
+
+<script>
+export default {
+  name: 'Settings',
+  data () {
+    return {
+      items: [
+        {
+          name: '鬧鈴',
+          file: 'alarm1.mp3'
+        },
+        {
+          name: 'yay',
+          file: 'yay.mp3'
+        }
+      ],
+      fields: [
+        {
+          key: 'name',
+          label: '名稱'
+        },
+        {
+          key: 'file',
+          label: '預覽'
+        },
+        {
+          key: 'select',
+          label: '選擇'
+        }
+      ]
+    }
+  },
+  computed: {
+    alarm () {
+      return this.$store.state.alarm
+    }
+  },
+  methods: {
+    selectAlarm (item) {
+      this.$store.commit('selectAlarm', item.file)
+      console.log(item)
+    }
+  }
+}
+</script>
